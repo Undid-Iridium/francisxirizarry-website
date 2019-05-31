7dbfe081536af4f4c95774ca7c3f713ce3281731
undid-iridium <francis.x.irizarry@gmail.com>
2019-05-31 15:10:25 -0400

    Updates

---
+++ precache-manifest.b6f9f8ec85d81361ca387b068c3f8cf7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49abf50c531a9ed309ba56a5bde77232",
+    "url": "/francisxirizarry-website/index.html"
+  },
+  {
+    "revision": "87b998f7e08db7adecb4",
+    "url": "/francisxirizarry-website/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/francisxirizarry-website/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "87b998f7e08db7adecb4",
+    "url": "/francisxirizarry-website/static/js/main.99bd1720.chunk.js"
+  },
+  {
+    "revision": "6c585a39901fea039e45",
+    "url": "/francisxirizarry-website/static/js/runtime~main.14123e8f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/francisxirizarry-website/static/media/logo.5d5d9eef.svg"
+  }
+]);
